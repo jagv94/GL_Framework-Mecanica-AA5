d828c804c05f93a01138bf967bc988f7107d830c
Adrián <joseadrian.guerra@enti.cat>
2021-06-09 11:21:39 +0200

    Coment sphere code

--- code/Sphere.cpp
+++ code/Sphere.cpp
@@ -1,36 +1,44 @@
 #include "Sphere.h"
 #include <iostream>
 
-void SphereClass::SolverEuler(glm::vec3 forceBuorancy, float _dt)
+int SphereClass::get_indexz(int _row, int _width, int _col)
 {
+	return _row * _width + _col;
+}
 
-	force = gravity + forceBuorancy;
+SphereClass::SphereClass(glm::vec3 _position, float _mass, float _radius) : position(_position), mass(_mass), radius(_radius) {}
+
+void SphereClass::SolverEuler(glm::vec3 _forceBuorancy, float _dt)
+{
+	force = gravity + _forceBuorancy;
 
 	position = position + velocity * _dt;
 	velocity = velocity + (force / mass) * _dt;
 
 }
 
-glm::vec3 SphereClass::CalculateBuoyancy(Mesh wave, float densityFluid, float _gravity)
+glm::vec3 SphereClass::CalculateBuoyancy(Mesh _wave, float _densityFluid, float _gravity)
 {
-	for (int i = 0; i < wave.height; i++)
+	for (int i = 0; i < _wave.height; i++)
 	{
-		for (int j = 0; j < wave.width; j++)
+		for (int j = 0; j < _wave.width; j++)
 		{
-			if (position.y - radius < wave.positions[get_indexz(i, wave.width, j)].y &&
-				position.x - radius < wave.positions[get_indexz(i, wave.width, j)].x && position.x + radius > wave.positions[get_indexz(i, wave.width, j)].x &&
-				position.z - radius < wave.positions[get_indexz(i, wave.width, j)].z && position.z + radius > wave.positions[get_indexz(i, wave.width, j)].z)
+			glm::vec3 wavePoints = _wave.positions[get_indexz(i, _wave.width, j)]; //Posicion de los puntos de la maya de la ola
+
+			//Calculamos la colision con la parte sumergida de la esfera con los puntos de la maya de la ola
+			if (position.y - radius < wavePoints.y &&
+				position.x - radius < wavePoints.x &&
+				position.x + radius > wavePoints.x &&
+				position.z - radius < wavePoints.z &&
+				position.z + radius > wavePoints.z)
 			{
-				float h = glm::length((position.y - radius) - wave.positions[get_indexz(i, wave.width, j)].y);
-				float volume = (glm::pi<float>() * glm::pow(h, 2) / 3.0f) * ((3.0f * radius) - (h));
+				float h = glm::length((position.y - radius) - _wave.positions[get_indexz(i, _wave.width, j)].y); //Altura sumergida de la esfera
+				float volume = (glm::pi<float>() * glm::pow(h, 2) / 3.0f) * ((3.0f * radius) - (h)); //Volumen sumergido de la esfera
 
-				//std::cout << volume << std::endl;
-
-				return (densityFluid * _gravity * volume * glm::vec3(0.0f, 1.0f, 0.0f));
+				return (_densityFluid * _gravity * volume * glm::vec3(0.0f, 1.0f, 0.0f)); //Devolvemos la flotabilidad de la esfera
 			}
 		}
 	}
-
 
 	return glm::vec3(0.0f, 0.0f, 0.0f);
 }
--- code/Sphere.h
+++ code/Sphere.h
@@ -6,26 +6,44 @@
 
 class SphereClass
 {
-	glm::vec3 position = { 0.0f, 0.0f, 0.0f };;
-	glm::vec3 velocity = { 0.0f, 0.0f, 0.0f };;
-	glm::vec3 force = { 0.0f, 0.0f, 0.0f };
-	glm::vec3 gravity = {0.0f, -9.81f, 0.0f};
+	glm::vec3 position = { 0.0f, 0.0f, 0.0f }; //Posicion inicial de la esfera
+	glm::vec3 velocity = { 0.0f, 0.0f, 0.0f }; //Velocidad inicial de la esfera
+	glm::vec3 force = { 0.0f, 0.0f, 0.0f };	//Fuerza inicial de la esfera
+	glm::vec3 gravity = { 0.0f, -9.81f, 0.0f }; //Gravedad
 
-	float mass;
+	float mass = 1.f; //Masa de la esfera
 
-	float radius = 1.0f;
+	float radius = 1.0f; //Radio de la esfera
 
-	int get_indexz(int row, int width, int col)
-	{
-		return row * width + col;
-	}
+	/// <summary>
+	/// Calcular la posicion de los puntos de la maya
+	/// </summary>
+	/// <param name="row">Particulas de la maya de las olas en horizontal</param>
+	/// <param name="width">Anchura de la maya</param>
+	/// <param name="col">Particulas de la maya de las olas en vertical</param>
+	/// <returns>Posición de un punto concreto de la maya</returns>
+	int get_indexz(int _row, int _width, int _col);
 
 public:
-	SphereClass(glm::vec3 _position, float _mass, float _radius) : position(_position), mass(_mass), radius(_radius){}
+	SphereClass(glm::vec3 _position, float _mass, float _radius);
 
-	void SolverEuler(glm::vec3 forceBuorancy, float _dt);
+	/// <summary>
+	/// Solver para el movimiento de la esfera
+	/// </summary>
+	/// <param name="_forceBuorancy">Resultado del calculo de la fuerza de flotabilidad</param>
+	/// <param name="_dt">Delta time</param>
+	void SolverEuler(glm::vec3 _forceBuorancy, float _dt);
+
+	/// <summary>
+	/// Calcular flotabilidad de la esfera
+	/// </summary>
+	/// <param name="_wave">Maya de la ola en uso</param>
+	/// <param name="_densityFluid">Densidad del fluido</param>
+	/// <param name="_gravity">Gravedad en uso</param>
+	/// <returns></returns>
+	glm::vec3 CalculateBuoyancy(Mesh _wave, float _densityFluid, float _gravity);
+	
 	glm::vec3 GetPosition() { return position; }
 	float GetMass() { return mass; }
-	glm::vec3 CalculateBuoyancy(Mesh wave, float densityFluid, float gravity);
 };
 
